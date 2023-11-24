360ae8e4a3077934d4779847f7dbeb7ed1c4d76f
GopalAror <punjabijanu586@gmail.com>
2023-11-24 15:37:39 +0530

    responsive half

--- main.js
+++ main.js
@@ -17,22 +17,22 @@ $('.slidr').slick({
   speed: 300,
   slidesToShow: 3,
   slidesToScroll: 1,
-  prevArrow:'.prev',
-  nextArrow:'.next',
+  prevArrow: '.prev',
+  nextArrow: '.next',
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
+        slidesToShow: 2,
         slidesToScroll: 1,
         infinite: true,
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 675,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToShow: 1,
+        slidesToScroll: 1
       }
     },
     {
